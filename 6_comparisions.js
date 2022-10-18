619ddc23a8c5c8492a0ea12b8a1e99e0cc6cd506
Govind <abhishek.govind@sap.com>
2022-10-18 15:18:40 +0530

    comaprisions ops

---
+++ 6_comparisions.js
@@ -0,0 +1,6 @@
+console.log("2" > 1); // consoles out the value true since the "2" string is converted to number (remember, only the unary operator is going to convert the number to string since string concatination is a thing)
+
+console.log((0 == false) == (1 === true));
+
+console.log(null == undefined);
+console.log(null === undefined);
