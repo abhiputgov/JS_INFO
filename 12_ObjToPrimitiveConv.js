06c9e54f3fe01182ac933dd3b2afec90aa7e893a
abhiputgov <abhipg@outlook.com>
2022-11-17 23:36:23 +0530

    object to primitive conversion part2

--- 12_ObjToPrimitiveConv.js
+++ 12_ObjToPrimitiveConv.js
@@ -1,4 +1,4 @@
-//object to number convresion
+//object to number conversion
 let user = {};
 
 let numObj = Number(user);
@@ -9,3 +9,20 @@ console.log(numObj);
 let user2 = {name: "Abhishek", age: 30};
 
 console.log(user2);
+
+//Symbol to primitive
+let user3 = {
+    name: "John",
+    money: 1000,
+
+    [Symbol.toPrimitive](hint){
+        console.log(`hint: ${hint}`);
+        return hint == "string" ? `{name: "${this.name}"}` : this.money;
+    }
+};
+
+console.log(user3);
+console.log(+user3);
+console.log(user3 + 500);
+
+
