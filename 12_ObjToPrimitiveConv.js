c68092bb5d5129e9bdbb21d85ccf2d785257a56d
abhiputgov <abhipg@outlook.com>
2022-11-17 23:20:39 +0530

    Object to Primitive conversion

---
+++ 12_ObjToPrimitiveConv.js
@@ -0,0 +1,11 @@
+//object to number convresion
+let user = {};
+
+let numObj = Number(user);
+
+console.log(numObj);
+
+// object to string conversion
+let user2 = {name: "Abhishek", age: 30};
+
+console.log(user2);
